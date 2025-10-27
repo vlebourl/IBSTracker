bbaaff3f6f40a034526349473231f09c5e9ecd67
Vincent Le Bourlot <vlb@starqube.com>
2025-10-27 15:00:45 +0100

    chore: Bump version to 1.11.1 (versionCode 15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "com.tiarkaerell.ibstracker"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.11.0"
+        versionCode = 15
+        versionName = "1.11.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
